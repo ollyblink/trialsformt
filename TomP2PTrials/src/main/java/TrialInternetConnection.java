ae895928833fadf65abd5070c2387f03e56f06df
ozihler <oliver_zihler@hotmail.com>
2015-11-16 10:11:58 +0100

    f

--- TomP2PTrials/src/main/java/TrialInternetConnection.java
+++ TomP2PTrials/src/main/java/TrialInternetConnection.java
@@ -1,25 +1,28 @@
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.Random;
 
 import net.tomp2p.connection.Bindings;
 import net.tomp2p.dht.PeerBuilderDHT;
 import net.tomp2p.dht.PeerDHT;
+import net.tomp2p.futures.FutureDiscover;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerBuilder;
 import net.tomp2p.peers.Number160;
+import net.tomp2p.peers.PeerAddress;
 import utils.GetOwnIpAddressTest;
 
 public class TrialInternetConnection {
 	public static void main(String[] args) {
 
-		int peerID = 1;
+		String ip = "192.168.43.234";
+		int port = 4000;
+		int peerID = 2;
 		if (peerID == 1) {
 			Bindings b = new Bindings();
 			Random RND = new Random();
 			b.addInterface("wlan0");
 
-			String ip = "192.168.43.234";
-			int port = 4000;
 			try {
 				GetOwnIpAddressTest.main(null);
 				Peer start = new PeerBuilder(Number160.createHash("super peer")).bindings(b).ports(port).behindFirewall(true).start();
