a0c982dff6da68cb647358fae5f1c110a647bdc5
ollyblink <oliver_zihler@hotmail.com>
2015-11-16 12:10:37 +0100

    fff

--- TomP2PTrials/src/main/java/TrialInternetConnection.java
+++ TomP2PTrials/src/main/java/TrialInternetConnection.java
@@ -1,28 +1,28 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.NavigableMap;
 import java.util.Random;
-import java.util.TreeMap;
 
 import net.tomp2p.connection.Bindings;
+import net.tomp2p.dht.FutureGet;
+import net.tomp2p.dht.FuturePut;
 import net.tomp2p.dht.PeerBuilderDHT;
 import net.tomp2p.dht.PeerDHT;
+import net.tomp2p.futures.BaseFutureListener;
 import net.tomp2p.futures.FutureBootstrap;
 import net.tomp2p.futures.FutureDiscover;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerBuilder;
 import net.tomp2p.peers.Number160;
-import net.tomp2p.peers.Number640;
 import net.tomp2p.peers.PeerAddress;
 import net.tomp2p.storage.Data;
 import utils.GetOwnIpAddressTest;
 
 public class TrialInternetConnection {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		String ip = "192.168.43.234";
 		int port = 4000;
-		int peerID = 3;
+		int peerID =3;
 		if (peerID == 1) {
 			Bindings b = new Bindings();
 			Random RND = new Random();
@@ -45,7 +45,6 @@ public class TrialInternetConnection {
 			try {
 				Peer peer = new PeerBuilder(Number160.createHash("client peer")).bindings(b).ports(port + 1).behindFirewall(true)
 						.broadcastHandler(new MyBroadcastHandler()).start();
-				PeerDHT peerDHT = new PeerBuilderDHT(peer).start();
 				PeerAddress bootstrap = new PeerAddress(Number160.ZERO, new InetSocketAddress(ip, port));
 				FutureDiscover futureDiscover = peer.discover().peerAddress(bootstrap).start();
 				futureDiscover.awaitUninterruptibly();
@@ -56,13 +55,47 @@ public class TrialInternetConnection {
 					bootstrap = futureDiscover.reporter();
 					FutureBootstrap fb = peer.bootstrap().peerAddress(bootstrap).start();
 					fb.awaitUninterruptibly();
+
+					PeerDHT peerDHT = new PeerBuilderDHT(peer).start();
 					if (fb.isSuccess()) {
 						Thread.sleep(4000);
-						if (peerID == 3) {
-							NavigableMap<Number640, Data> dataMap = new TreeMap<Number640, Data>();
-							Number160 hash = Number160.createHash("Hello World Broadcast");
-							dataMap.put(new Number640(hash, hash, hash, hash), new Data("Hello World Broadcast"));
-							peer.broadcast(hash).dataMap(dataMap).start();
+						String key = "Hello World";
+						if (peerID == 2) {
+//							NavigableMap<Number640, Data> dataMap = new TreeMap<Number640, Data>();
+//							Number160 hash = Number160.createHash("Hello World Broadcast");
+//							dataMap.put(new Number640(hash, hash, hash, hash), new Data("Hello World Broadcast"));
+//							peer.broadcast(hash).dataMap(dataMap).start();
+							FuturePut start = peerDHT.put(Number160.createHash(key)).data(new Data(key+" 1")).start();
+							start.addListener(new BaseFutureListener<FuturePut>() {
+
+								@Override
+								public void operationComplete(FuturePut future) throws Exception {
+									if(future.isSuccess()){
+										System.out.println("Successful put");
+									}
+								}
+
+								@Override
+								public void exceptionCaught(Throwable t) throws Exception {
+									t.printStackTrace();
+								}
+							});
+						}else if(peerID == 3){
+							FutureGet get = peerDHT.get(Number160.createHash(key)).start();
+							get.addListener(new BaseFutureListener<FutureGet>() {
+
+								@Override
+								public void operationComplete(FutureGet future) throws Exception {
+									if(future.isSuccess()){
+										System.out.println("Got: " +future.data().object());
+									}
+								}
+
+								@Override
+								public void exceptionCaught(Throwable t) throws Exception {
+									t.printStackTrace();
+								}
+							});
 						}
 					}
 				} else {
