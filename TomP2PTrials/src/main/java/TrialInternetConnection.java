b27e4f9692c39b088fa91c805c245962b1fe9779
ollyblink <oliver_zihler@hotmail.com>
2015-11-16 14:27:54 +0100

    asdfasdf

--- TomP2PTrials/src/main/java/TrialInternetConnection.java
+++ TomP2PTrials/src/main/java/TrialInternetConnection.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.NavigableMap;
 import java.util.Random;
+import java.util.TreeMap;
 
 import net.tomp2p.connection.Bindings;
 import net.tomp2p.dht.FutureGet;
@@ -13,6 +15,7 @@ import net.tomp2p.futures.FutureDiscover;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerBuilder;
 import net.tomp2p.peers.Number160;
+import net.tomp2p.peers.Number640;
 import net.tomp2p.peers.PeerAddress;
 import net.tomp2p.storage.Data;
 import utils.GetOwnIpAddressTest;
@@ -27,13 +30,15 @@ public class TrialInternetConnection {
 		GetOwnIpAddressTest.main(null);
 		Bindings b = new Bindings();
 		b.addInterface("wlan0");
-		 Random RND = new Random();
+		Random RND = new Random();
 		if (peerID == 1) {
-
-			new PeerBuilder(Number160.createHash("super peer")).bindings(b).ports(port).start();
+ 			new PeerBuilder(Number160.createHash("super peer")).bindings(b).ports(port).start();
 
 		} else {
-			Peer myPeer = new PeerBuilder(Number160.createHash("client peer")).bindings(b).behindFirewall(true).ports(port+RND.nextInt(1000)+1).enableMaintenance(false).start();
+			int port2 = port + RND.nextInt(1000) + 1;
+			System.out.println("Port 2: " +port2);
+			Peer myPeer = new PeerBuilder(Number160.createHash("client peer")).broadcastHandler(new MyBroadcastHandler()).bindings(b)
+					.behindFirewall(true).ports(port2).enableMaintenance(false).start();
 			PeerAddress bootstrapServerPeerAddress = new PeerAddress(Number160.ZERO, new InetSocketAddress(InetAddress.getByName(ipSuperPeer), port));
 
 			FutureDiscover discovery = myPeer.discover().peerAddress(bootstrapServerPeerAddress).start();
@@ -42,7 +47,7 @@ public class TrialInternetConnection {
 				System.err.println("no success!");
 			}
 			System.err.println("Peer: " + discovery.reporter() + " told us about our address.");
-			InetSocketAddress myInetSocketAddress = new InetSocketAddress(myPeer.peerAddress().inetAddress(), port);
+			InetSocketAddress myInetSocketAddress = new InetSocketAddress(myPeer.peerAddress().inetAddress(), port2);
 
 			bootstrapServerPeerAddress = discovery.reporter();
 			FutureBootstrap bootstrap = myPeer.bootstrap().peerAddress(bootstrapServerPeerAddress).start();
@@ -51,21 +56,25 @@ public class TrialInternetConnection {
 			if (!bootstrap.isSuccess()) {
 				System.err.println("no success!");
 			}
-
-			PeerDHT myPeerDHT = new PeerBuilderDHT(myPeer).start();
-
-			FuturePut putFuture = myPeerDHT.put(Number160.createHash("key")).data(new Data(myInetSocketAddress)).start();
-			putFuture.awaitUninterruptibly();
-			FutureGet futureDHT = myPeerDHT.get(Number160.createHash("key")).start();
-			futureDHT.awaitUninterruptibly();
-			futureDHT.futureRequests().awaitUninterruptibly();
-			Data data = futureDHT.data();
-			if (data == null) {
-				throw new RuntimeException("Address not available in DHT.");
-			}
-			InetSocketAddress inetSocketAddress = (InetSocketAddress) data.object();
-			System.err.println("returned " + inetSocketAddress);
-//			myPeer.shutdown();
+//
+//			Number160 hash = Number160.createHash("Hello");
+//			NavigableMap<Number640, Data> dataMap = new TreeMap<Number640, Data>();
+//			dataMap.put(new Number640(hash, hash, hash, hash), new Data("Broadcast hello world"));
+//			myPeer.broadcast(hash).dataMap(dataMap).start();
+			 PeerDHT myPeerDHT = new PeerBuilderDHT(myPeer).start();
+			
+			 FuturePut putFuture = myPeerDHT.put(Number160.createHash("key")).data(new Data(myInetSocketAddress)).start();
+			 putFuture.awaitUninterruptibly();
+			 FutureGet futureDHT = myPeerDHT.get(Number160.createHash("key")).start();
+			 futureDHT.awaitUninterruptibly();
+			 futureDHT.futureRequests().awaitUninterruptibly();
+			 Data data = futureDHT.data();
+			 if (data == null) {
+			 throw new RuntimeException("Address not available in DHT.");
+			 }
+			 InetSocketAddress inetSocketAddress = (InetSocketAddress) data.object();
+			 System.err.println("returned " + inetSocketAddress);
+//			// myPeer.shutdown();
 		}
 	}
 }
