d3764c43eea4872021f85cf503c210f83ba63d6b
ozihler <oliver_zihler@hotmail.com>
2015-11-16 09:13:24 +0100

    some useful classes

---
+++ TomP2PTrials/src/main/java/utils/ListAvailableNetworkInterfaces.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+
+@SuppressWarnings("PMD")
+public class ListAvailableNetworkInterfaces {
+
+    public static void main(String args[]) throws SocketException {
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets)) {
+            displayInterfaceInformation(netint);
+        }
+    }
+
+    static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+        System.out.printf("Display name: %s\n", netint.getDisplayName());
+        System.out.printf("Name: %s\n", netint.getName());
+        System.out.printf("HW Address: %s\n", Arrays.toString(netint.getHardwareAddress()));
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            System.out.printf("InetAddress: %s\n", inetAddress);
+        }
+        System.out.printf("\n");
+    }
+}
