35edd53e7edd4d75cf58f481c939c39ffe3fd7cf
ollyblink <oliver_zihler@hotmail.com>
2015-11-16 10:25:52 +0100

    fff

--- TomP2PTrials/src/main/java/TrialInternetConnection.java
+++ TomP2PTrials/src/main/java/TrialInternetConnection.java
@@ -7,6 +7,7 @@ import java.util.TreeMap;
 import net.tomp2p.connection.Bindings;
 import net.tomp2p.dht.PeerBuilderDHT;
 import net.tomp2p.dht.PeerDHT;
+import net.tomp2p.futures.FutureBootstrap;
 import net.tomp2p.futures.FutureDiscover;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerBuilder;
@@ -17,11 +18,11 @@ import net.tomp2p.storage.Data;
 import utils.GetOwnIpAddressTest;
 
 public class TrialInternetConnection {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		String ip = "192.168.43.234";
 		int port = 4000;
-		int peerID = 1;
+		int peerID = 3;
 		if (peerID == 1) {
 			Bindings b = new Bindings();
 			Random RND = new Random();
@@ -40,23 +41,29 @@ public class TrialInternetConnection {
 		} else {
 
 			Bindings b = new Bindings();
-			b.addInterface("wlan1");
+			b.addInterface("wlan0");
 			try {
-				Peer peer = new PeerBuilder(Number160.createHash("client peer")).bindings(b).ports(port).behindFirewall(true)
+				Peer peer = new PeerBuilder(Number160.createHash("client peer")).bindings(b).ports(port + 1).behindFirewall(true)
 						.broadcastHandler(new MyBroadcastHandler()).start();
 				PeerDHT peerDHT = new PeerBuilderDHT(peer).start();
 				PeerAddress bootstrap = new PeerAddress(Number160.ZERO, new InetSocketAddress(ip, port));
 				FutureDiscover futureDiscover = peer.discover().peerAddress(bootstrap).start();
 				futureDiscover.awaitUninterruptibly();
 				if (futureDiscover.isSuccess()) {
-					System.out.println("Successfully connected");
-					
-					if(peerID == 3) {
-						
-						NavigableMap<Number640, Data> dataMap = new TreeMap<Number640, Data>();
-						Number160 hash = Number160.createHash("Hello World Broadcast");
-						dataMap.put(new Number640(hash,hash,hash,hash),new Data("Hello World Broadcast"));
-						peer.broadcast(hash).dataMap(dataMap).start();
+					System.out.println("Successfully discovered");
+					InetSocketAddress myInetSocketAddress = new InetSocketAddress(peer.peerAddress().inetAddress(), port);
+
+					bootstrap = futureDiscover.reporter();
+					FutureBootstrap fb = peer.bootstrap().peerAddress(bootstrap).start();
+					fb.awaitUninterruptibly();
+					if (fb.isSuccess()) {
+						Thread.sleep(4000);
+						if (peerID == 3) {
+							NavigableMap<Number640, Data> dataMap = new TreeMap<Number640, Data>();
+							Number160 hash = Number160.createHash("Hello World Broadcast");
+							dataMap.put(new Number640(hash, hash, hash, hash), new Data("Hello World Broadcast"));
+							peer.broadcast(hash).dataMap(dataMap).start();
+						}
 					}
 				} else {
 					System.out.println("Could not connect");
